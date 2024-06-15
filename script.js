ab4396731049c4e025c9f58851484350475332de
tripathiharsh95 <tripathiharsh088@gmail.com>
2024-06-15 21:36:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-let string = "";
-let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
-  button.addEventListener('click', (e)=>{
-    if(e.target.innerHTML == '='){
-      string = eval(string);
-      document.querySelector('input').value = string;
-    }
-    else if(e.target.innerHTML == 'C'){
-      string = ""
-      document.querySelector('input').value = string;
-    }
-    else{ 
-    console.log(e.target)
-    string = string + e.target.innerHTML;
-    document.querySelector('input').value = string;
-      }
-  })
-})
+function Solve(val) {
+    var v = document.getElementById('res');
+    v.value += val;
+ }
+ function Result() {
+    var num1 = document.getElementById('res').value;
+    var num2 = eval(num1);
+    document.getElementById('res').value = num2;
+ }
+ function Clear() {
+    var inp = document.getElementById('res');
+    inp.value = '';
+ }
+ function Back() {
+    var ev = document.getElementById('res');
+    ev.value = ev.value.slice(0,-1);
+ }
